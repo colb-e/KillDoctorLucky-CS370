813e80b2a398156869bd1c3616c44220df99cb68
ZheniaO <ochoaz@sunypoly.edu>
2024-02-28 17:57:26 -0500

    Made buttons and added a quit button

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ screen_W, screen_L = screen.get_size()
 screen = pygame.display.set_mode((screen_W,screen_L))
 
 # Game board
-board_W = screen_W
+board_W = screen_W * 0.9
 board_L = screen_L
 board = pygame.image.load("images/Board2.jpg").convert()
 board = pygame.transform.scale(board, (board_W,board_L)).convert()
@@ -26,9 +26,30 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
+# Character Images
+fay = "images/fay-chanceworthy-pixilart.png"
+drlucky = "images/doctor-lucky-pixilart.png"
+
 # Buttons
-nextTurnImage = pygame.image.load("images/next_turn.png")
-nextTurnButton = button.Button(nextTurnImage, 0, 0)
+sidebar_W = screen_W - board_W
+
+nextTurnImage = pygame.image.load("images/nextturnbutton.png")
+btn_scale = 1.5 # will change the Length of the button
+nextTurn_W, nextTurn_L = nextTurnImage.get_size() # grabbing image size
+nextTurn_W = sidebar_W # setting the width of the image to the width of the sidebar
+nextTurn_l = (nextTurn_L* btn_scale) # multiplying given scale with the image length
+nextTurnimage = pygame.transform.scale(nextTurnImage, (nextTurn_W, nextTurn_L)) # adjusting to new image size
+nextTurn_X, nextTurn_Y = (screen_W - nextTurn_W) // 1, (screen_L - nextTurn_L) // 1000 # placing the button in a relative Location the // 1 and the // 1000 decide the placement
+nextTurnButton = button.Button(nextTurnImage, nextTurn_X, nextTurn_Y) # calling the button class
+
+quitImage = pygame.image.load("images/quitbutton.png")
+btn_scale = 1.5 # change the Length of the button
+quit_W, quit_L = quitImage.get_size() # grabbing image size
+quit_W = sidebar_W # setting the width of the image to the width of the sidebar
+quit_l = (quit_L* btn_scale) # multiplying given scale with the image length
+quitimage = pygame.transform.scale(quitImage, (quit_W, quit_L)) # adjusting to new image size
+quit_X, quit_Y = (screen_W - quit_W) // 1, (screen_L - quit_L) // 1 # placing the button in a relative Location the // 1 and the // 1 decide the placement
+quitButton = button.Button(quitImage, quit_X, quit_Y) # calling the button class
 
 # Players
 
@@ -36,25 +57,25 @@ nextTurnButton = button.Button(nextTurnImage, 0, 0)
 
 
 drLucky = player.Player(screen, BLACK, 0, 2, 1.8) # drawing dr lucky in room 1
-player1 = player.Player(screen, RED, 0, 2, 1.8)
-player2 = player.Player(screen, BLUE, 0, 2, 1.8)
-player3 = player.Player(screen, GREEN, 0, 2, 1.8)
+player_1 = player.Player(screen, RED, 0, 2, 1.8)
+player_2 = player.Player(screen, BLUE, 0, 2, 1.8)
+player_3 = player.Player(screen, GREEN, 0, 2, 1.8)
 
 
 playerList = []
 turnOrder = 0
 roomCount = 0
 playerList.append(drLucky)
-playerList.append(player1)
-playerList.append(player2)
-playerList.append(player3)
+playerList.append(player_1)
+playerList.append(player_2)
+playerList.append(player_3)
 
 TEMP = 23
 
 drLucky.updatePlayer(TEMP) # updating player to index 1 in roomList (room 2)
-player1.updatePlayer(TEMP)
-player2.updatePlayer(TEMP)
-player3.updatePlayer(TEMP)
+player_1.updatePlayer(TEMP)
+player_2.updatePlayer(TEMP)
+player_3.updatePlayer(TEMP)
 drLucky.updatePlayer(TEMP) # updating player to index 1 in roomList (room 2)
 drLucky.updatePlayer(TEMP) # updating player to index 1 in roomList (room 2)
 while True:
@@ -74,7 +95,7 @@ while True:
     # Draw objects to screen
     screen.blit(board,(0,0))
     
-    # if the button is clicked
+    # if the next turn button is clicked
     if nextTurnButton.drawButton(screen) == True:
         
         print("Moved player ", turnOrder)
@@ -106,10 +127,16 @@ while True:
             turnOrder += 1
 
 
+# if the quit button is clicked
+    if quitButton.drawButton(screen) == True:
+            pygame.quit()
+            running = False
+            exit()
+
     drLucky.DrawPlayer()
-    player1.DrawPlayer()
-    player2.DrawPlayer()
-    player3.DrawPlayer()
+    player_1.DrawPlayer()
+    player_2.DrawPlayer()
+    player_3.DrawPlayer()
 
     pygame.display.update()
     clock.tick(60)
